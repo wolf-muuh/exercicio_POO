de803772e32fae8037c2989bfb3f33870063b94f
Murilo Alves Zapiello <54750759805@info.local>
2023-04-26 15:37:43 -0300

    finalização

--- Celular.cs
+++ Celular.cs
@@ -14,12 +14,51 @@ namespace exercicio_POO
 
         public void Ligar()
         {
-            Console.WriteLine($"Deseja ligar o seu celular?");
+            string opcao = "";
+            do
+            {
+
+
+                Console.WriteLine($"Deseja ligar o seu celular? digite s para sim e n para não");
+                opcao = Console.ReadLine().ToLower();
+                switch (opcao)
+                {
+                    case "s":
+                        Console.WriteLine($"Celular ligado!");
+                        Ligado = true;
+                        break;
+                    case "n":
+                        Console.WriteLine($"programa encerrado");
+                        Ligado = false;
+
+                        break;
+
+                    default:
+                        Console.WriteLine($"Digite uma opção válida");
+
+                        break;
+                }
+            } while (opcao != "s" && opcao != "n");
+
 
         }
         public void Desligar()
         {
-            Console.WriteLine($"Deseja desligar o seu celular?");
+            string opcao0 = "";
+            do
+            {
+
+
+                Console.WriteLine($"Deseja desligar o seu celular?Digite s para sim e n para não");
+                opcao0 = Console.ReadLine().ToLower();
+                if (opcao0 == "s")
+                {
+                    Console.WriteLine($"Fim do programa. Tenha um bom dia!");
+
+                }
+
+
+            } while (opcao0 != "s");
         }
         public void FazerLigação()
         {
@@ -30,6 +69,7 @@ namespace exercicio_POO
                 case "s":
                     Console.WriteLine($"Digite o número de telefone");
                     string num = Console.ReadLine();
+                    Console.WriteLine($"Fazendo uma ligação para {num}");
                     break;
                 case "n":
                     Console.WriteLine($"Saindo do modo ligação...");
@@ -38,11 +78,81 @@ namespace exercicio_POO
                     Console.WriteLine($"Opção inválida!");
                     break;
             }
+
         }
         public void EnviarMensagem()
         {
-            Console.WriteLine($"Deseja enviar uma mensagem?");
-            
+            string opcao2 = "";
+            do
+            {
+
+
+                Console.WriteLine($"Deseja enviar uma mensagem? digite s para sim e n para não");
+                opcao2 = Console.ReadLine().ToLower();
+                switch (opcao2)
+                {
+                    case "s":
+                        string nomMsg = "";
+                        do
+                        {
+                            Console.WriteLine($"Digite o nome do contato que você deseja enviar uma mensagem: ");
+                            nomMsg = Console.ReadLine();
+                            if (nomMsg == "")
+                            {
+                                Console.WriteLine($"ERRO! Espaço em branco!");
+
+                            }
+                        } while (nomMsg == "");
+                        string msg = "";
+
+                        do
+                        {
+
+
+                            Console.WriteLine($"Digite sua mensagem para {nomMsg}: ");
+                            msg = Console.ReadLine();
+                            if (msg == "")
+                            {
+                                Console.WriteLine($"ERRO! Espaço em branco!");
+
+                            }
+
+                            else
+                            {
+                                string opcao3 = "";
+                                do
+                                {
+
+
+                                    Console.WriteLine($"Sua mensagem é: {msg} deseja envia-la? digite s para sim n para não");
+                                    opcao3 = Console.ReadLine().ToLower();
+                                    switch (opcao3)
+                                    {
+                                        case "s":
+                                            Console.WriteLine($"Mensagem para {nomMsg} enviada com sucesso, a mensagem foi: {msg}");
+
+                                            break;
+                                        case "n":
+                                            Console.WriteLine($"Mensagem para {nomMsg} não enviada!");
+                                            break;
+
+                                        default:
+                                            Console.WriteLine($"Selecione uma das opções válidas");
+
+                                            break;
+                                    }
+                                } while (opcao3 == "");
+
+                            }
+                        } while (msg == "");
+
+                        break;
+                    default:
+                        Console.WriteLine($"Selecione uma das opções válidas");
+
+                        break;
+                }
+            } while (opcao2 == "");
 
         }
     }
--- Program.cs
+++ Program.cs
@@ -2,3 +2,67 @@
 // Com os métodos, ligar, desligar, fazer ligação, enviar mensagem.
 // Só será possível executar tais métodos se o celular estiver ligado.
 // Envie o link do repositório como entrega desta atividade.
+using exercicio_POO;
+Celular celular = new Celular();
+
+Console.WriteLine($"Bem vindo ao sistema mundial de celular!");
+Console.WriteLine(@$"
+Digite as seguintes especificações do seu celular:
+Cor;
+Modelo;
+Tamanho.
+");
+celular.Cor = Console.ReadLine();
+celular.Modelo = Console.ReadLine();
+celular.Tamanho = Console.ReadLine();
+
+celular.Ligar();
+
+
+if (celular.Ligado == true)
+{
+    string OPCAO = "";
+    do
+    {
+
+
+        Console.WriteLine(@$"
+    Escolhas uma das funcionalidades do seu celular através dos números que a representa:
+    1 - Ligar 
+    2 - mandar mensagem 
+    3 - especificações do aparelho
+    0 - sair
+    ");
+        OPCAO = Console.ReadLine();
+        switch (OPCAO)
+        {   
+            case "1":
+                celular.FazerLigação();
+                break;
+
+            case "2":
+                celular.EnviarMensagem();
+                break;
+            case "3":
+                Console.WriteLine(@$"
+            Especificações:
+            Cor: {celular.Cor};
+            Modelo: {celular.Modelo};
+            Tamanho: {celular.Tamanho}.
+            ");
+
+                break;
+            case "0":
+                celular.Desligar();
+                break;
+            default:
+                Console.WriteLine($"Digite uma opção válida.");
+                break;
+        }
+    } while (OPCAO != "0");
+
+
+}
+
+
+
